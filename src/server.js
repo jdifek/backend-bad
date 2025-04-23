f47e05c9d293adce416053fab7a5797f8edbe12e
jdifek <givotkov11@gmail.com>
2025-04-23 21:51:21 +0300

    wqwq

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
