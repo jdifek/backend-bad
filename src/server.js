c9bb6e32cc9dd26d1b4e5151d85ad7636ae3438b
jdifek <givotkov11@gmail.com>
2025-04-23 22:04:22 +0300

    wqwq

--- src/server.js
+++ src/server.js
@@ -1,19 +1,8 @@
-const express = require('express');
-require('dotenv').config();
+const app = require('./app')
+require('dotenv').config()
 
-const app = express();
+const PORT = process.env.PORT || 5000
 
-app.use((req, res, next) => {
-  console.log(`[${req.method}] ${req.url}`);
-  next();
-});
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-const PORT = process.env.PORT;
-
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`)
+})
